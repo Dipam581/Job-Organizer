a2a644b8ec5653b36d696f5caba26cb88445e0f6
Dipam Ghosh <107043079+Dipam581@users.noreply.github.com>
2024-04-12 15:30:07 +0530

    Update Request.js

--- frontend/src/components/BackendResponse/Request.js
+++ frontend/src/components/BackendResponse/Request.js
@@ -2,25 +2,29 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 function Request() {
-    function sendData() {
-        const data = { key: 'value' };
-        axios.post('http://127.0.0.1:8000/api/', data)
-            .then((response) => {
-                console.log('Success:', response.data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }
-    useEffect(() => {
-        sendData();
-    }, [])
+  function sendData() {
+    const data = { key: 'value' };
+    
+    axios.post('http://127.0.0.1:8000/api/', JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then((response) => {
+      console.log('Success:', response.data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
 
-    return (
-        <>
-            <div>Check console for response.</div>
-        </>
-    )
+  useEffect(() => {
+    sendData();
+  }, []); // The empty array as second argument means this effect runs once after initial render
+
+  return (
+    <div>Check console for response.</div>
+  );
 }
 
-export default Request
+export default Request;
